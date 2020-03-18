354bdc5d929a2d15fd34a5e33d7da8e3a08951c2
Julian Liebich <Julian.Liebich@consol.de>
2020-03-18 12:32:36 +0100

    Adding tests for basic xanthippe functionality.

---
+++ core_functionality.test.js
@@ -0,0 +1,29 @@
+const xant = require('./core_functionality.js');
+
+// test("Sample test: Hello World!", () => {
+//     expect(testFunction("2 + 2 = 4", 2 + 2 === 4)).toBe(true);
+// });
+
+describe('Testing core functionality of xanthippe', () => {
+
+    it('performIO should simply return its input values (true).', () => {
+        expect(xant.performIO('',true)).toBe(true);
+    });
+
+    it('performIO should simply return its input values (false).', () => {
+        expect(xant.performIO('',false)).toBe(false);
+    });
+
+    it('assertTrue should return true if given function returns true.', () => {
+        expect(xant.assertTrue('should be true', () => {return true;})).toBe(true);
+    });
+
+    it('assertIntEquals should return true', () => {
+        expect(xant.assertIntEquals('', 1234, 1234)).toBe(true);
+    })
+
+    it('assertIntEquals should return false', () => {
+        expect(xant.assertIntEquals('', 1234, 1235)).toBe(false);
+    })
+
+});
