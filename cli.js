d598f5caf20dc1b1cfb84ecc84552cbfee4adcab
YunaAmnell <marlene.kermani@consol.de>
2020-03-23 13:29:09 +0100

    (#8) Xanthippe can now output input parameters to cli

--- cli.js
+++ cli.js
@@ -1,3 +1,14 @@
 #! /usr/bin/env node
 
+//Arguments
+const [,, ... args] = process.argv;
+let index = 0;
+
+//Log
 console.log("Xanthippe starting...");
+console.log(`Input parameters: `);
+args.forEach(element => {
+    index++;
+    console.log(` ${index}: ${element}`)
+    
+});
