7ec47b58eb4c7c3dad3f7f0695a494d733dbfb61
YunaAmnell <marlene.kermani@consol.de>
2020-03-23 12:15:27 +0100

    (#4) Refactored code for assertion testing

--- assertions.test.js
+++ assertions.test.js
@@ -1,41 +1,91 @@
 const xant = require('./core_functionality.js');
 
+console.log = jest.fn();
+
 describe('Testing assertions', () => {
 
     it('assertTrue should return true if given function returns true.', () => {
+        // GIVEN
         const myfn = function () {
             return false;
         }
-        expect(() => xant.assertTrue('should be true', myfn)).toThrow();
-    });
 
+        // WHEN
+        const SUT = () => xant.assertTrue('should be true', myfn);
+
+        // THEN
+        expect(SUT).toThrow();
+        expect(console.log).toHaveBeenCalledWith('should be true');
+    });
 
     describe('Testing assertEquals function.', () => {
 
         it('Comparing two naive string values', () => {
-            expect(xant.assertEquals('hallo', 'hallo')).toBeUndefined();
+            // GIVEN
+
+
+            // WHEN
+            const SUT = () => xant.assertEquals('hallo', 'hallo');
+
+            // THEN
+            expect(SUT()).toBeUndefined();
         });
 
         it('Comparing two naive integers.', () => {
-            expect(xant.assertEquals(1, 1)).toBeUndefined();
+            // GIVEN
+
+            // WHEN
+            const SUT = () => xant.assertEquals(1, 1);
+
+            // THEN
+            expect(SUT()).toBeUndefined();
         });
 
         it('Comparing two naive integers, should fail', () => {
+            // GIVEN
             let variable2 = 3;
-            expect(() => xant.assertEquals(1, variable2)).toThrow();
+
+            // WHEN
+            const SUT = () => xant.assertEquals(1, variable2);
+            
+            // THEN
+            expect(SUT).toThrow();
         });
 
         it('Comparing flat object.', () => {
+            // GIVEN
             let object2 = { val1: 1, val2: 2 };
-            expect(xant.assertEquals({ val1: 1, val2: 2 }, object2)).toBeUndefined();
+
+            // WHEN
+            const SUT = () => xant.assertEquals({ val1: 1, val2: 2 }, object2);
+
+            // THEN
+            expect(SUT()).toBeUndefined();
         });
 
         it('Comparing nested object', () => {
-            expect(xant.assertEquals({ val1: 1, val2: { val: 1 } }, { val1: 1, val2: { val: 1 } })).toBeUndefined();
+            // GIVEN
+            const obj1 =  {val1: 1, val2: { val: 1 }};
+            const obj2 =  { val1: 1, val2: { val: 1 }};
+
+            // WHEN
+            const SUT = () => xant.assertEquals(obj1,obj2);
+
+            // THEN
+
+            expect(SUT()).toBeUndefined();
         });
 
         it('Comparing nested object', () => {
-            expect(() => xant.assertEquals({ val1: 1, val2: { val: 1 } }, { val1: 1, val2: { val: 2 } })).toThrow();
+            // GIVEN
+            const obj1 = { val1: 1, val2: { val: 1 } };
+            const obj2 = { val1: 1, val2: { val: 2 } };
+
+            // WHEN
+            const SUT = () => xant.assertEquals(obj1,obj2);
+
+            // THEN
+            expect(SUT).toThrow();
         });
     });
 });
