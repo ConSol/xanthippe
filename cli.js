c2dbdf1fef23da4a6ec234ac60d3c09f78aa52bc
Julian Liebich <Julian.Liebich@consol.de>
2020-03-24 13:26:44 +0100

    (#8) cli can now run test files where no import of xant is required.

--- cli.js
+++ cli.js
@@ -1,5 +1,12 @@
 #! /usr/bin/env node
 
+const vm = require('vm');
+const xant = require('./core_functionality.js');
+const fs = require('fs');
+
+//global.xant = xant;
+
+const context = {};
 //Arguments
 const [,, ... args] = process.argv;
 let index = 0;
@@ -11,4 +18,15 @@ args.forEach(element => {
     index++;
     console.log(` ${index}: ${element}`)
     
+});
+
+args.forEach(filepath => {
+    console.log('Running testsuite: ' + filepath);
+    const code = fs.readFileSync(filepath, 'utf-8');
+    //console.log(code);
+    //const script = new vm.Script(code);
+    const context = {xant : xant, console: console};
+    vm.createContext(context);
+    vm.runInContext(code, context);
+    //console.log(context);
 });
---
+++ test_cli.js
@@ -0,0 +1,7 @@
+xant.testcase('1 should be 1', () => {
+    xant.assertEquals(1,1);
+    //xant.assertTrue();
+
+});
+
+//console.log("heellllloooo");
