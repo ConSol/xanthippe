c44ae114ad3163e3fcfe22aa6c6cfdee92a7fa57
YunaAmnell <marlene.kermani@consol.de>
2020-04-01 15:02:17 +0200

    (#11) function for listing directory recursively

---
+++ listDirRec.js
@@ -0,0 +1,17 @@
+const fs = require('fs');
+//function for listing directory recursively
+function listDirRec(path) {
+    let paths = [];
+    let directoryContent = fs.readdirSync(path);
+    directoryContent.forEach((element) => {
+        const stat = fs.statSync(path + "/" + element);
+        if (!stat.isDirectory()) {
+            paths.push(path + "/" + element);
+        }
+        else {
+            paths = [...paths, ...listDirRec(path + "/" + element)];
+        }
+    });
+    return paths;
+}
+exports.listDirRec = listDirRec;
