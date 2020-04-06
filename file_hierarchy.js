8ac8b5e6d2866dd5faec404bde1d1a13cdb5922b
Julian Liebich <Julian.Liebich@consol.de>
2020-04-06 16:14:06 +0200

    (#12) changed return behaviour of directory; more descriptive symbol names.

--- file_hierarchy.js
+++ file_hierarchy.js
@@ -2,49 +2,52 @@ const fs = require('fs');
 const path = require('path');
 const os = require('os');
 
-function mkdir(dirname, callback) {
-    let oldpwd = this.PWD;
-    if (this.PWD === undefined) {
+function directory(dirname, callback) {
+    let oldWorkingDirectory = this.workingDirectory;
+    console.log(oldWorkingDirectory);
+    let ret = "";
+    if (this.workingDirectory === undefined) {
         //let basepath = path.join(os.tmpdir(), 'xanthippe');
         let tempdir = fs.mkdtempSync(path.join(os.tmpdir(), 'xanthippe'));
         //let tempdir = fs.mkdirSync(path.join(os.tmpdir(), 'xanthippe'));
-        console.log(tempdir);
+        //console.log(tempdir);
         fs.mkdirSync(path.join(tempdir, dirname));
-        this.PWD = path.join(tempdir, dirname);
-        console.log(this.PWD);
+        this.workingDirectory = path.join(tempdir, dirname);
+        //console.log(this.workingDirectory);
+        ret = tempdir;
     } else {
-        this.PWD = path.join(this.PWD, dirname);
+        this.workingDirectory = path.join(this.workingDirectory, dirname);
+        ret = this.workingDirectory;
     }
-    console.log(this.PWD);
+    console.log(this.workingDirectory);
     try {
-        let snc = fs.statSync(this.PWD);
-        if (!snc.isDirectory(this.PWD)) {
-            /// create directory this.PWD
-            fs.mkdirSync(this.PWD);
-            //console.log('create direcotry: ' + this.PWD);
+        let snc = fs.statSync(this.workingDirectory);
+        if (!snc.isDirectory(this.workingDirectory)) {
+            /// create directory this.workingDirectory
+            fs.mkdirSync(this.workingDirectory);
+            //console.log('create direcotry: ' + this.workingDirectory);
         }
     } catch (e) {
-        fs.mkdirSync(this.PWD);
+        fs.mkdirSync(this.workingDirectory);
     }
 
     callback();
-    const ret = this.PWD;
-    this.PWD = oldpwd;
+    this.workingDirectory = oldWorkingDirectory;
     return ret;
 }
 
-function touch(filename, text = 'hallo') {
-    let filepath = path.join(this.PWD, filename);
+function file(filename, text = 'hallo') {
+    let filepath = path.join(this.workingDirectory, filename);
     try {
         let snc = fs.statSync(filepath);
-        console.log(`cannot create file: object with name ${filename} already exists in ${this.PWD}`);
+        console.log(`cannot create file: object with name ${filename} already exists in ${this.workingDirectory}`);
         // if (!fs.statSync(filepath).isFile()) {
         //     fs.writeFileSync(filepath, text);
-        //     console.log('create file: ' + path.join(this.PWD, filename));
+        //     console.log('create file: ' + path.join(this.workingDirectory, filename));
         
     } catch (e) {
         fs.writeFileSync(filepath, text);
-        console.log('create file: ' + path.join(this.PWD, filename));
+        console.log('create file: ' + path.join(this.workingDirectory, filename));
     }
 
 }
