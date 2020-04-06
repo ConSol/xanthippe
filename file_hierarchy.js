46d7956a908d3c8785488f01ef89264aa814d231
Julian Liebich <Julian.Liebich@consol.de>
2020-04-06 13:48:30 +0200

    (#12) prototype for file hierarchy feature.

---
+++ file_hierarchy.js
@@ -0,0 +1,71 @@
+const fs = require('fs');
+const path = require('path');
+const os = require('os');
+
+function mkdir(dirname, callback) {
+    let oldpwd = this.PWD;
+    if (this.PWD === undefined) {
+        //let basepath = path.join(os.tmpdir(), 'xanthippe');
+        let tempdir = fs.mkdtempSync(path.join(os.tmpdir(), 'xanthippe'));
+        //let tempdir = fs.mkdirSync(path.join(os.tmpdir(), 'xanthippe'));
+        console.log(tempdir);
+        fs.mkdirSync(path.join(tempdir, dirname));
+        this.PWD = path.join(tempdir, dirname);
+        console.log(this.PWD);
+    } else {
+        this.PWD = path.join(this.PWD, dirname);
+    }
+    console.log(this.PWD);
+    try {
+        let snc = fs.statSync(this.PWD);
+        if (!snc.isDirectory(this.PWD)) {
+            /// create directory this.PWD
+            fs.mkdirSync(this.PWD);
+            //console.log('create direcotry: ' + this.PWD);
+        }
+    } catch (e) {
+        fs.mkdirSync(this.PWD);
+    }
+
+    callback();
+    this.PWD = oldpwd;
+}
+
+function touch(filename, text = 'hallo') {
+    let filepath = path.join(this.PWD, filename);
+    try {
+        let snc = fs.statSync(filepath);
+        console.log(`cannot create file: object with name ${filename} already exists in ${this.PWD}`);
+        // if (!fs.statSync(filepath).isFile()) {
+        //     fs.writeFileSync(filepath, text);
+        //     console.log('create file: ' + path.join(this.PWD, filename));
+        
+    } catch (e) {
+        fs.writeFileSync(filepath, text);
+        console.log('create file: ' + path.join(this.PWD, filename));
+    }
+
+}
+
+
+mkdir('hello', () => {
+    mkdir('hello2', () => {
+        touch('hallo.txt');
+    });
+    mkdir('hello3', () => {
+        touch('hallo.txt');
+        touch('hallo2.txt');
+        touch('hallo2.txt');
+    });
+    mkdir('hello3', () => {
+        touch('halloolal');
+        touch('hallo2.txt');
+    });
+    touch('hello3');
+});
+
+mkdir('helo', () => {
+    mkdir('helo2', () => {
+        touch('helo.js');
+    });
+});
