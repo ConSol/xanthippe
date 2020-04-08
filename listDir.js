771685095b4cae224dc723a6f4871342d5697bfb
Julian Liebich <Julian.Liebich@consol.de>
2020-04-08 12:58:32 +0200

    (#11) listDir doesnt filter out directory paths.

--- listDir.js
+++ listDir.js
@@ -3,21 +3,36 @@ const { join } = require("path");
 
 //function for listing directory recursively
 function listDir(path, recurse = false) {
-    if( fs.statSync(path).isDirectory() ){
-        return fs.readdirSync(path).map(elem => {
-            if (recurse){
-                return listDir(join(path,elem),true);
-            } else {
-                if ( !fs.statSync(join(path,elem)).isDirectory() ){
-                    return [join(path,elem)];
-                } else {
-                    return [];
-                }
-            }
-        }).reduce((akk, elem) => akk.concat(elem), []);
-    } else {
+    if (!fs.statSync(path).isDirectory()) {
         return [path];
     }
+
+    // if (recurse){
+    //     return fs.readdirSync(path).map(elem => listDir(join(path,elem), recurse));
+    // } else {
+    //     return fs.readdirSync(path).map(elem => {
+    //         if (!fs.statSync(join(path, elem)).isDirectory()) {
+    //                         return [join(path, elem)];
+    //                     } else {
+    //                         return [];
+    //                     }
+    //     });
+    // }
+
+    return fs.readdirSync(path).map(elem => {
+        const pathAbsolute = join(path,elem);
+        if (recurse) {
+            return listDir(pathAbsolute, recurse);
+        } else {
+            return [pathAbsolute];
+            // if (!fs.statSync(pathAbsolute).isDirectory()) {
+            //     return listDir(pathAbsolute, recurse);
+            //     // return [pathAbsolute];
+            // } else {
+            //     return [];
+            // }
+        }
+    }).reduce((akk, elem) => akk.concat(elem), []);
 }
 
 exports.listDir = listDir;
