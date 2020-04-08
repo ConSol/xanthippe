d312c09ad62490642c4775c43a3e578d94dd3fa7
Julian Liebich <Julian.Liebich@consol.de>
2020-04-08 11:46:31 +0200

    (#11) deleting obsolete comment.

--- listDir.js
+++ listDir.js
@@ -17,21 +17,6 @@ function listDir(path, recurse = false) {
     } else {
         return [path];
     }
-
-    // let paths = [];
-    // let directoryContent = fs.readdirSync(path);
-    // directoryContent.forEach((element) => {
-    //     const stat = fs.statSync(join(path, element));
-    //     if (!stat.isDirectory()) {
-    //         paths.push(join(path, element));
-    //     }
-    //     else {
-    //         if(recurse){
-    //             paths = [...paths, ...listDir(join(path, element), true)];
-    //         }
-    //     }
-    // });
-    // return paths;
 }
 
 exports.listDir = listDir;
