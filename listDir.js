1be2f20eaa16740c843359a57008299a6e1fd2bb
YunaAmnell <marlene.kermani@consol.de>
2020-04-08 13:22:27 +0200

    (#11) function listDir does not filter out directories anymore if no recursion wanted

--- listDir.js
+++ listDir.js
@@ -7,30 +7,12 @@ function listDir(path, recurse = false) {
         return [path];
     }
 
-    // if (recurse){
-    //     return fs.readdirSync(path).map(elem => listDir(join(path,elem), recurse));
-    // } else {
-    //     return fs.readdirSync(path).map(elem => {
-    //         if (!fs.statSync(join(path, elem)).isDirectory()) {
-    //                         return [join(path, elem)];
-    //                     } else {
-    //                         return [];
-    //                     }
-    //     });
-    // }
-
     return fs.readdirSync(path).map(elem => {
-        const pathAbsolute = join(path,elem);
+        const pathAbsolute = join(path, elem);
         if (recurse) {
             return listDir(pathAbsolute, recurse);
         } else {
             return [pathAbsolute];
-            // if (!fs.statSync(pathAbsolute).isDirectory()) {
-            //     return listDir(pathAbsolute, recurse);
-            //     // return [pathAbsolute];
-            // } else {
-            //     return [];
-            // }
         }
     }).reduce((akk, elem) => akk.concat(elem), []);
 }
