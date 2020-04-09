9679a983c3ba65ad08b550467a5a5d581dde0497
YunaAmnell <marlene.kermani@consol.de>
2020-04-09 15:24:27 +0200

    (#12) added test for directory function in file_hierarchy

--- file_hierarchy.test.js
+++ file_hierarchy.test.js
@@ -1,9 +1,15 @@
 const {directory} = require('./file_hierarchy');
 const {join} = require('path');
 const {tmpdir} = require('os');
+const fs = require('fs');
+
+const mymkdtempSync = fs.mkdtempSync;
+fs.mkdtempSync = jest.fn((elem) => mymkdtempSync(elem));
+//fs.mkdirSync = jest.fn();
+
 
 describe('directory', () => {
-    it('should return a temporary xanthippe directory', () => {
+    /* it('should return a temporary xanthippe directory', () => {
         //GIVEN
         const tempdirname = "1234"
         const path = "".concat(join(tmpdir(), 'xanthippe').split("\\").map(elem => {
@@ -22,5 +28,18 @@ describe('directory', () => {
 
         //THEN
         expect(rootdir).toEqual(expect.stringMatching(expected));
-    }) 
-})
+    })  */
+
+    it('should return a temporary xanthippe directory', () => {
+        //GIVEN
+        const call = join(tmpdir(), 'xanthippe');
+    
+
+        //WHEN
+        directory('1234', () => console.log('hallo'));
+
+
+        //THEN
+        expect(fs.mkdtempSync).toHaveBeenCalledWith(call);
+    }
+)})
