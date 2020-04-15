1bf5c51b4888a8b7cdd086077181a50fc803e008
Julian Liebich <Julian.Liebich@consol.de>
2020-04-15 15:30:01 +0200

    (#12) directory returns object.

--- file_hierarchy.js
+++ file_hierarchy.js
@@ -5,15 +5,16 @@ const os = require('os');
 function directory(dirname, callback) {
     let oldWorkingDirectory = this.workingDirectory;
     console.log(oldWorkingDirectory);
-    let ret = "";
     if (this.workingDirectory === undefined) {
+        //
         let tempdir = fs.mkdtempSync(path.join(os.tmpdir(), 'xanthippe'));
         fs.mkdirSync(path.join(tempdir, dirname));
         this.workingDirectory = path.join(tempdir, dirname);
-        ret = tempdir;
+        //ret = tempdir;
+        //
     } else {
         this.workingDirectory = path.join(this.workingDirectory, dirname);
-        ret = this.workingDirectory;
+
     }
     console.log(this.workingDirectory);
     try {
@@ -26,6 +27,16 @@ function directory(dirname, callback) {
     }
 
     callback();
+    const directoryPath = this.workingDirectory;
+    const ret = {
+        dir: this.workingDirectory,
+        deleteTree: () => {
+            const delpath = path.join(os.tmpdir(), directoryPath.split(path.sep).find((elem) => {
+                return elem.match(/^xanthippe\w{6}$/);
+            }));
+            fs.rmdirSync(delpath, { recursive: true });
+        }
+    };
     this.workingDirectory = oldWorkingDirectory;
     return ret;
 }
@@ -34,7 +45,7 @@ function file(filename, text = 'hallo') {
     let filepath = path.join(this.workingDirectory, filename);
     try {
         let snc = fs.statSync(filepath);
-        console.error(`cannot create file: object with name ${filename} already exists in ${this.workingDirectory}`);        
+        console.error(`cannot create file: object with name ${filename} already exists in ${this.workingDirectory}`);
     } catch (e) {
         fs.writeFileSync(filepath, text);
         console.log('create file: ' + path.join(this.workingDirectory, filename));
@@ -42,14 +53,14 @@ function file(filename, text = 'hallo') {
 
 }
 
-function cleanAll(){
+function cleanAll() {
     const tmpDirContents = fs.readdirSync(os.tmpdir());
     tmpDirContents.filter((cpath) => {
-       if( fs.statSync(os.tmpdir(), cpath).isDirectory() && cpath.match(/^xanthippe\w{6}$/) ){
+        if (fs.statSync(os.tmpdir(), cpath).isDirectory() && cpath.match(/^xanthippe\w{6}$/)) {
             return true;
         } else {
-           return false;
-       }
+            return false;
+        }
     }).forEach((elem) => {
         fs.rmdirSync(path.join(os.tmpdir(), elem), { recursive: true });
     });
@@ -61,8 +72,7 @@ module.exports = {
     cleanAll
 }
 
-
-/* const dirdir = directory('hello', () => {
+const dirdir = directory('hello', () => {
     directory('hello2', () => {
         file('hallo.txt');
     });
@@ -76,13 +86,6 @@ module.exports = {
         file('hallo2.txt');
     });
     file('hello3');
-    console.log(somedir);
-}); */
-//fs.rmdirSync(dirdir, { recursive: true });
-// nix mehr da
+});
 
-// directory('helo', () => {
-//     directory('helo2', () => {
-//         file('helo.js');
-//     });
-// });
+dirdir.deleteTree();
