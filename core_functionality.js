e24e43cfe265fe085ef930e0bafb27d747275ee2
YunaAmnell <marlene.kermani@consol.de>
2020-04-17 13:30:59 +0200

    (#13) added beforeEach and afterEach function

--- core_functionality.js
+++ core_functionality.js
@@ -2,21 +2,44 @@ const xant_assert = require('./assertions.js');
 const xant_expect = require('./expect.js');
 const chalk = require('chalk');
 
-function testcase(description, callback){
+//Global variables
+let beforeEachFunction;
+let afterEachFunction;
+
+function testcase(description, callback) {
     console.log('Running test: ' + description);
     try {
+        if (beforeEachFunction !== undefined) {
+            beforeEachFunction();
+        }
         callback();
-    } catch (e){
+
+        if (afterEachFunction !== undefined) {
+            afterEachFunction();
+        }
+    } catch (e) {
         console.error(chalk.red.inverse.bold("... test failed!"));
         throw e;
     }
     console.log(chalk.green.inverse.bold("... test succeeded"));
 }
 
+function beforeEach(callback) {
+    beforeEachFunction = callback;
+}
+
+function afterEach(callback) {
+    afterEachFunction = callback;
+}
+
+
+
 module.exports = {
     assertEquals: xant_assert.assertEquals,
     assertTrue: xant_assert.assertTrue,
     testcase,
     it: testcase,
-    expect: xant_expect.expect
+    expect: xant_expect.expect,
+    beforeEach,
+    afterEach
 };
