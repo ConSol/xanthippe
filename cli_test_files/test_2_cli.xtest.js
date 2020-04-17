4be69ea5c99bb9cd43b8656765d80c6e8ee7503c
YunaAmnell <marlene.kermani@consol.de>
2020-04-17 13:40:12 +0200

    (#13) added beforeEach and afterEach to user testcase

--- cli_test_files/test_2_cli.xtest.js
+++ cli_test_files/test_2_cli.xtest.js
@@ -8,6 +8,17 @@ const obj2 = {
     var2: 2
 }
 
+beforeEach(() => console.log("I wanna be called before each testcase"));
+afterEach(() => console.log("I wanna be called after each testcase"));
+
+testcase('1 should be 1', () => {
+    assertEquals(obj1,obj2);
+});
+
+testcase('1 should be 1', () => {
+    assertEquals(obj1,obj2);
+});
+
 testcase('1 should be 1', () => {
     assertEquals(obj1,obj2);
 });
