6d08a75a4f6fdf989f93e74eae947b397e0fd45b
YunaAmnell <marlene.kermani@consol.de>
2020-04-29 15:25:58 +0200

    (#12) added new function filestructure

--- file_hierarchy.js
+++ file_hierarchy.js
@@ -2,6 +2,51 @@ const fs = require('fs');
 const path = require('path');
 const os = require('os');
 
+const layout = {
+    
+    'directory1' : {
+        'directory2' : {
+            'file3' : 'content'
+        },
+
+        'file2' : 'content'
+    },
+
+    'file1' : 'content'
+}
+
+
+function filestructure(layout, workingDirectory){
+    let dirdir;
+    
+
+    if(workingDirectory === undefined ){
+        workingDirectory = fs.mkdtempSync(path.join(os.tmpdir(), 'xanthippe'));
+    } 
+
+
+    for(let [key,value] of Object.entries(layout)){
+        console.log('key: ' + key);
+        console.log('value: ' + value);
+        if(typeof value === 'object'){
+            dirdir = directory(key, (wd) => {
+                console.log("created dir");
+                console.log('currentDir: ' + wd);
+                filestructure(value, wd);
+        }, workingDirectory);
+        } else if(typeof value === 'string') {
+            console.log('key: ' + key);
+            console.log('value: ' + value);
+            dirdir = file(key,value,workingDirectory);
+        }
+   
+    }
+
+    return dirdir;
+    
+}
+
+
 function directory(dirname, callback, workingDirectory) {
     if (dirname === '') {
         throw "Name of directory must not be empty!";
@@ -41,6 +86,11 @@ function directory(dirname, callback, workingDirectory) {
 }
 
 function file(filename, text = 'hallo', workingDirectory) {
+
+    if(workingDirectory === undefined ){
+        workingDirectory = fs.mkdtempSync(path.join(os.tmpdir(), 'xanthippe'));
+    }
+
     let filepath = path.join(workingDirectory, filename);
     try {
         let snc = fs.statSync(filepath);
@@ -49,6 +99,19 @@ function file(filename, text = 'hallo', workingDirectory) {
         fs.writeFileSync(filepath, text);
         console.log('create file: ' + path.join(workingDirectory, filename));
     }
+
+    const ret = {
+        dir: workingDirectory,
+        deleteTree: () => {
+            fs.rmdirSync(ret.getRoot(), { recursive: true });
+        },
+        getRoot: () => {
+            return path.join(os.tmpdir(), workingDirectory.split(path.sep).find((elem) => {
+                return elem.match(/^xanthippe\w{6}$/);
+            }));
+        }
+    };
+    return ret;
 
 }
 
@@ -68,5 +131,9 @@ function cleanAll() {
 module.exports = {
     directory,
     file,
-    cleanAll
+    cleanAll,
+    filestructure
 }
+
+
+filestructure(layout)
